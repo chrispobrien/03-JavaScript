564f3710f6a0cf1e70c3cab7f6ab38f4454a8778
chrispobrien <chris.obrien57@gmail.com>
2022-04-26 21:51:15 -0400

    One bug fix and minor refactoring

--- script.js
+++ script.js
@@ -17,20 +17,19 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
 
   // We have 4 yes/no questions, so I have this local function
+  // returns y, n, or null
   function promptYN(promptText,defaultValue) {
     // Declare return variable yn as local block level
-    let yn=defaultValue;
-    yn = yn.toLowerCase;
+    let yn=defaultValue.toLowerCase;
 
-    // Check defaultValue is a valid choice, if not set to y (yes)
-    if (yn != 'y' && yn != 'n') {yn=""};
+    // Check defaultValue is a valid choice, if not set to y (yes) arbitrarily
+    if (defaultValue != 'y' && defaultValue != 'n') {defaultValue='y'};
 
     // Loop until user submits a valid response or cancels the prompt
     do {
-      yn = prompt(promptText,defaultValue);
-      yn = yn.toLowerCase();
-      if (yn != 'y' && yn !='n' && yn != null) {
-        alert("Please enter y or n!");
+      yn = prompt(promptText,defaultValue).toLowerCase();
+      if (yn != 'y' && yn != 'n' && yn != null) {
+        alert('Please enter y or n!');
       }
     } while (yn != 'y' && yn != 'n' && yn != null);
     return yn;
@@ -54,33 +53,27 @@ function generatePassword() {
   // Repeat prompt until number entered within minimum and maximum, default to minimum
   // Note: prompt returns a string value, test that the data entered is an integer by comparing it to its parseInt() value
   do {
-    length = prompt("How long do you want your password to be? (" + minLength + " - " + maxLength + ")", minLength);
+    length = prompt(`How long do you want your password to be? (${minLength} - ${maxLength})`, minLength);
     if (length === null) return("");
     if (length < minLength || length > maxLength || length != parseInt(length))
-     alert("Please enter a valid number between "+ minLength + " and " + maxLength + "!");
+     alert(`Please enter a valid number between ${minLength} and ${maxLength}!`);
   } while (length < minLength || length > maxLength || length != parseInt(length));
   // Cast length to an integer just in case an integer within a string causes problems later on
   length = parseInt(length);
 
-  // Initialize options as empty strings
-  let includeUpperCase="";
-  let includeLowerCase="";
-  let includeNumbers="";
-  let includeSymbols="";
-
   // Repeat prompts until at least one character type selected or user cancels
   do {
 
-    includeUpperCase = promptYN("Include upper case letters (A-Z)? (y/n)", "y");
+    var includeUpperCase = promptYN("Include upper case letters (A-Z)? (y/n)", "y");
     if (includeUpperCase === null) return("");
 
-    includeLowerCase = promptYN("Include lower case letters (a-z)? (y/n)", "y");
+    var includeLowerCase = promptYN("Include lower case letters (a-z)? (y/n)", "y");
     if (includeLowerCase === null) return("");
 
-    includeNumbers = promptYN("Include numbers (0-9)? (y/n)", "y");
+    var includeNumbers = promptYN("Include numbers (0-9)? (y/n)", "y");
     if (includeNumbers === null) return("");
 
-    includeSymbols = promptYN("Include symbols? (y/n)", "y");
+    var includeSymbols = promptYN("Include symbols? (y/n)", "y");
     if (includeSymbols === null) return("");
 
     // If user doesn't select at least one character type, show error
@@ -88,9 +81,6 @@ function generatePassword() {
       alert("Please choose at least one character type!");
 
   } while (includeUpperCase === "n" && includeLowerCase === "n" && includeNumbers === "n" && includeSymbols === "n");
-
-  // Declare return var as empty string
-  let returnPassword="";
 
   // Assemble keySpace to include all selected character types
   let keySpace="";
@@ -108,6 +98,8 @@ function generatePassword() {
     keySpace += symbols;
   
   // Finally generate desired length password using random characters from keySpace
+  // Declare return var as empty string
+  let returnPassword="";
   for (let i = 0; i < length; i++) {
     returnPassword += keySpace[Math.floor(Math.random()*keySpace.length)];
   }
